e850ab8187ad4b29e672944e29ed7225de2f553c
Praneen Shakya <praneen.shakya@introcept.co>
2019-07-10 16:04:06 +0545

    Adjust capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,13 @@
 lock "~> 3.11.0"
 
 set :application, "awsTest"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "git@github.com:praneenshakya/awsTest.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/var/www/awsTest"
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
